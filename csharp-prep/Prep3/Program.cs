ffdc357ef3fa0cd6c6369eda29ab4835baa9c7f4
Hallway5 <bruecopper2001@gmail.com>
2025-01-16 14:42:28 -0700

    Magic Number prep

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        // string magicNumberString = Console.ReadLine();
+        // int magicNumber = int.Parse(magicNumberString);
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 21);
+
+        Console.Write("What is your guess? ");
+        string guessString = Console.ReadLine();
+        int guess = int.Parse(guessString);
+
+        while (guess != magicNumber){
+            if (guess < magicNumber){
+
+                Console.WriteLine("Higher");
+            } else if (guess > magicNumber){
+
+                Console.WriteLine("Lower");
+            }
+            Console.Write("What is your guess? ");
+            guessString = Console.ReadLine();
+            guess = int.Parse(guessString);
+        }
+        Console.WriteLine("You guessed it!");
     }
 }
